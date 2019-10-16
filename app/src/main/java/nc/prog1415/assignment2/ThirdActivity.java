521365737933f0950402f82268ad8a7cefd0dc99
svanderest1 <svanderest1@ncstudents.niagaracollege.ca>
2019-10-16 11:41:59 -0400

    finished media

--- app/src/main/java/nc/prog1415/assignment2/ThirdActivity.java
+++ app/src/main/java/nc/prog1415/assignment2/ThirdActivity.java
@@ -2,11 +2,14 @@ package nc.prog1415.assignment2;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class ThirdActivity extends Activity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,6 +21,15 @@ public class ThirdActivity extends Activity {
             public void onClick(View view) {
                 Intent i = new Intent(ThirdActivity.this, MainActivity.class);
                 startActivity(i);
+            }
+        });
+
+        final Button playButton = (Button)this.findViewById(R.id.playButton);
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MediaPlayer mp = MediaPlayer.create(ThirdActivity.this, R.raw.deepspace);
+                mp.start();
             }
         });
     }
